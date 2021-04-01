f7ed1db6db064bb75fe4c6d3d636b2662b2bcce3
Michael Bishop <cleverca22@gmail.com>
2021-03-31 21:53:36 -0300

    [libc] allow arch string rules to be missing [elf] add the constant for VC4

--- lib/elf/elf.c
+++ lib/elf/elf.c
@@ -159,6 +159,12 @@ static int verify_eheader(const void *header)
 #elif ARCH_MICROBLAZE
     if (eheader->e_machine != EM_MICROBLAZE)
         return ERR_NOT_FOUND;
+#elif ARCH_RISCV
+    if (eheader->e_machine != EM_RISCV)
+        return ERR_NOT_FOUND;
+#elif ARCH_VPU
+    if (eheader->e_machine != EM_VC4)
+        return ERR_NOT_FOUND;
 #else
 #error find proper EM_ define for your machine
 #endif
