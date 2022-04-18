85449ba989a1aaaa52cdc2ca141c15d721aae0f7
Travis Geiselbrecht <geist@foobox.com>
2022-04-18 00:45:07 -0700

    [app][mem_test] print the range before starting the test

--- app/tests/mem_tests.c
+++ app/tests/mem_tests.c
@@ -97,6 +97,9 @@ static void do_mem_tests(void *ptr, size_t len)
 {
     size_t i;
 
+    printf("running memory tests on address range [%p..%p]\n",
+            ptr, (uint8_t *)ptr + len - 1);
+
     /* test 1: simple write address to memory, read back */
     printf("test 1: simple address write, read back\n");
     volatile uint32_t *vbuf32 = ptr;
