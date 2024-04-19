8bd3f68f3989c8a791c4fec93fc068e30a861abc
Cody Wong <codycswong@google.com>
2024-04-19 18:42:22 +0800

    [fs][v9fs] Add VirtIO 9p filesystem structure
    Add the fundamental filesystem structure to attach a VirtualIO 9p
    device. With the implementation of VirtIO 9p devices (lk/dev/virtio/9p),
    we can use those APIs to connect to a shared folder as a LK filesystem.
    
    Signed-off-by: Cody Wong <codycswong@google.com>

--- platform/qemu-virt/rules.mk
+++ platform/qemu-virt/rules.mk
@@ -33,6 +33,10 @@ MODULE_DEPS += \
     dev/virtio/block \
     dev/virtio/gpu \
     dev/virtio/net \
+    dev/virtio/9p \
+    lib/cbuf \
+    lib/fdtwalk \
+    lib/fs/9p \
 
 GLOBAL_DEFINES += \
     MEMBASE=$(MEMBASE) \
