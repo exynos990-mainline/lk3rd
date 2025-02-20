70a4c3439dc2a0afd7e21843a019e6db5d19f4fb
Umer Uddin <umer.uddin@mentallysanemainliners.org>
2025-02-20 21:43:16 +0000

    Project: lk3rd-build-all: Add tar file creation
    We already make boot images, lets make tars too,
    to make our and other peoples lives a bit easier.
    
    Signed-off-by: Umer Uddin <umer.uddin@mentallysanemainliners.org>

--- lk3rd-build-all.sh
+++ lk3rd-build-all.sh
@@ -2,9 +2,24 @@
 set -e
 
 ./build.sh c1s
+cp boot-c1s.img boot.img
+tar -cvf boot-c1s.tar boot.img
 ./build.sh c2s
+cp boot-c2s.img boot.img
+tar -cvf boot-c2s.tar boot.img
 ./build.sh r8s
+cp boot-r8s.img boot.img
+tar -cvf boot-r8s.tar boot.img
 ./build.sh x1s
+cp boot-x1s.img boot.img
+tar -cvf boot-x1s.tar boot.img
 ./build.sh y2s
+cp boot-y2s.img boot.img
+tar -cvf boot-y2s.tar boot.img
 ./build.sh z3s
+cp boot-z3s.img boot.img
+tar -cvf boot-z3s.tar boot.img
+
+rm boot.img
+
 echo All done!
