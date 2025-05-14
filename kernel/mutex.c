cf290824311764183647e464a11b3556125c8910
Umer Uddin <umer.uddin@mentallysanemainliners.org>
2025-05-14 22:31:15 +0100

    Revert "kernel: mutex: Don't allow mutex_acquire with interrupts disabled"
    This reverts commit 10b23c738f75f4d8f9ebd67d358aca9eec5d7809.

--- kernel/mutex.c
+++ kernel/mutex.c
@@ -20,19 +20,6 @@
 #include <assert.h>
 #include <lk/err.h>
 #include <kernel/thread.h>
-#include <lk/init.h>
-
-static bool mutex_threading_ready;
-
-/* mutex_threading_ready is currently only used from a DEBUG_ASSERT */
-#if LK_DEBUGLEVEL > 1
-
-static void mutex_threading_ready_init_func(uint level) {
-    mutex_threading_ready = true;
-}
-
-LK_INIT_HOOK(mutex_threading_ready, mutex_threading_ready_init_func, LK_INIT_LEVEL_THREADING);
-#endif
 
 /**
  * @brief  Initialize a mutex_t
@@ -84,7 +71,6 @@ status_t mutex_acquire_timeout(mutex_t *m, lk_time_t timeout)
         panic("mutex_acquire_timeout: thread %p (%s) tried to acquire mutex %p it already owns.\n",
               get_current_thread(), get_current_thread()->name, m);
 #endif
-    DEBUG_ASSERT(!mutex_threading_ready || !timeout || !arch_ints_disabled());
 
     THREAD_LOCK(state);
 
