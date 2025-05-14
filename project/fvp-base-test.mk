b804ee8787b01e78597ad3aa198631f5abad0ced
Umer Uddin <umer.uddin@mentallysanemainliners.org>
2025-05-14 22:31:15 +0100

    Revert "Add FVP Base platform"
    This reverts commit 3179e41706006e79483025bab51fa9f99e5970b9.

--- project/fvp-base-test.mk
+++
@@ -1,7 +0,0 @@
-# main project for fvp-base
-MODULES += \
-  app/shell \
-
-include project/virtual/test.mk
-include project/target/fvp-base.mk
-
--- project/target/fvp-base.mk
+++
@@ -1,4 +0,0 @@
-# main project for fvp-base-aarch64
-ARCH := arm64
-ARM_CPU := cortex-a53
-TARGET := fvp-base
