f5481a44c24d56bd537b9215f0ba1066b6c25475
Igor Belwon <igor.belwon@mentallysanemainliners.org>
2025-05-28 12:51:02 +0200

    exynos_boot: mainline_boot: Add ramdisk nodes on boot

--- app/exynos_boot/boot/mainline_boot.c
+++ app/exynos_boot/boot/mainline_boot.c
@@ -20,12 +20,14 @@
 #include <dev/usb/gadget.h>
 #include <platform/mmu/mmu_func.h>
 #include <lib/font_display.h>
+#include <lib/fdtapi.h>
 
 #include <lk3rd/boot_reason.h>
 
 /* Hacky. */
 void arm_generic_timer_disable(void);
 int cmd_scatter_load_boot(int argc, const cmd_args *argv);
+extern struct fdt_header *fdt_dtb;
 
 void print_mainline_warning(void)
 {
@@ -33,34 +35,64 @@ void print_mainline_warning(void)
 	for (int i = 0; i < 3; i++)
 		print_lcd_update(FONT_RED, FONT_BLACK, "");
 
-	print_lcd_update(FONT_RED,    FONT_BLACK, "You are using mainline quirks.");
-	print_lcd_update(FONT_RED,    FONT_BLACK, "These quirks should not be used for booting Android.");
-	print_lcd_update(FONT_RED,    FONT_BLACK, "If you are booting Android, please reboot to fastboot and run");
+	print_lcd_update(FONT_RED, FONT_BLACK, "You are using mainline quirks.");
+	print_lcd_update(FONT_RED, FONT_BLACK, "These quirks should not be used for booting Android.");
+	print_lcd_update(FONT_RED, FONT_BLACK, "If you are booting Android, please reboot to fastboot and run");
 	print_lcd_update(FONT_YELLOW, FONT_BLACK, "fastboot oem disable-mainline-quirks");
 }
 
-void mainline_boot(void)
+int add_dt_ramdisk(uint32_t ramdisk_size)
+{
+	int offset, ret;
+
+	fdt_dtb = (struct fdt_header *)DT_BASE;
+
+	ret = fdt_check_header(fdt_dtb);
+	if (ret != 0)
+		goto err;
+
+	// Resize the DTB to fit the ramdisk properties
+	resize_dt(SZ_4K);
+
+	// Make /chosen node
+	offset = fdt_path_offset(fdt_dtb, "/");
+	if (offset < 0)
+		goto err;
+
+	ret = fdt_add_subnode(fdt_dtb, offset, "chosen");
+
+	// Make the rest
+	ret = set_fdt_val("/chosen", "bootargs", "root=/dev/mem0 initrd=0x84000000,0x1000000");
+	if (ret != 0)
+		goto err;
+
+	ret += set_fdt_val("/chosen", "linux,initrd-start", "<0x84000000>");
+	if (ret != 0)
+		goto err;
+
+	ret += set_fdt_val("/chosen", "linux,initrd-end", "<0x84FFFFFF>");
+	if (ret != 0)
+		goto err;
+
+	return 0;
+
+err:
+	printf("Failed to add ramdisk props: %s\n", fdt_strerror(ret));
+	return ret;
+}
+
+void mainline_boot_common(boot_img_hdr *b_hdr)
 {
 	int ret;
-	struct pit_entry *ptn;
 	cmd_args argv[6];
-	boot_img_hdr *boot_image = (boot_img_hdr *)BOOT_BASE;
 
-	print_mainline_warning();
-
-	ptn = pit_get_part_info("boot");
-	if (ptn == 0) {
-		printf("Partition 'boot' does not exist\n");
-		return;
-	} else {
-		pit_access(ptn, PIT_OP_LOAD, (u64)BOOT_BASE, 0);
-	}
-
-	if(strncmp((char*)boot_image->magic, BOOT_MAGIC, 8))
+	if (strncmp((char *)b_hdr->magic, BOOT_MAGIC, 8))
 	{
-		enter_reason = (char *)"boot failure!";
+		enter_reason = (char*)"Boot image magic not found.";
 		start_usb_gadget();
-		while(1){}
+		while (1)
+		{
+		}
 	}
 
 	argv[1].u = BOOT_BASE;
@@ -70,11 +102,27 @@ void mainline_boot(void)
 	argv[5].u = 0;
 	cmd_scatter_load_boot(6, argv);
 
-	ret = fdt_check_header((void*)DT_BASE);
-	if (ret) {
+	ret = fdt_check_header((void *)DT_BASE);
+	if (ret)
+	{
 		printf("libfdt fdt_check_header(): %s\n", fdt_strerror(ret));
 		return;
 	}
+
+	if (b_hdr->ramdisk_size != 0)
+	{
+		ret = add_dt_ramdisk(b_hdr->ramdisk_size);
+		if(ret != 0)
+		{
+			snprintf(enter_reason, ENTER_REASON_SIZE, "add_dt_ramdisk: %s", fdt_strerror(ret));
+			start_usb_gadget();
+			while (1)
+			{
+			}
+		}
+	}
+	else
+		printf("No ramdisk - skipping.\n");
 
 	/* notify EL3 Monitor end of bootloader */
 	exynos_smc(SMC_CMD_END_OF_BOOTLOADER, 0, 0, 0);
@@ -96,11 +144,30 @@ void mainline_boot(void)
 	return;
 }
 
+void mainline_boot(void)
+{
+	struct pit_entry *ptn;
+	boot_img_hdr *b_hdr = (boot_img_hdr *)BOOT_BASE;
+
+	print_mainline_warning();
+
+	ptn = pit_get_part_info("boot");
+	if (ptn == 0)
+	{
+		printf("Partition 'boot' does not exist\n");
+		return;
+	}
+	else
+	{
+		pit_access(ptn, PIT_OP_LOAD, (u64)BOOT_BASE, 0);
+	}
+
+	mainline_boot_common(b_hdr);
+}
+
 void mainline_boot_fb_boot(unsigned long buf_addr, size_t size)
 {
 	struct boot_img_hdr *b_hdr;
-	cmd_args argv[6];
-	int ret = 0;
 
 	print_mainline_warning();
 
@@ -110,42 +177,5 @@ void mainline_boot_fb_boot(unsigned long buf_addr, size_t size)
 
 	stop_usb_gadget();
 
-	if(strncmp((char*)b_hdr->magic, BOOT_MAGIC, 8))
-	{
-		enter_reason = (char *)"boot failure!";
-		start_usb_gadget();
-		while(1){}
-	}
-
-	argv[1].u = BOOT_BASE;
-	argv[2].u = KERNEL_BASE;
-	argv[3].u = RAMDISK_BASE;
-	argv[4].u = DT_BASE;
-	argv[5].u = 0;
-	cmd_scatter_load_boot(6, argv);
-
-        ret = fdt_check_header((void*)DT_BASE);
-        if (ret) {
-                printf("libfdt fdt_check_header(): %s\n", fdt_strerror(ret));
-                return;
-        }
-
-        /* notify EL3 Monitor end of bootloader */
-        exynos_smc(SMC_CMD_END_OF_BOOTLOADER, 0, 0, 0);
-
-        /* before jumping to kernel. disable interrupts */
-        arch_disable_ints();
-
-        /* before jumping to kernel. disble arch_timer */
-        arm_generic_timer_disable();
-
-	clean_invalidate_dcache_all();
-	disable_mmu_dcache();
-
-        void (*kernel_entry)(int r0, int r1, int r2, int r3);
-        kernel_entry = (void (*)(int, int, int, int))KERNEL_BASE;
-        kernel_entry(DT_BASE, 0, 0, 0);
-
-        /* We shouldn't get here. */
-        return;
+	mainline_boot_common(b_hdr);
 }
