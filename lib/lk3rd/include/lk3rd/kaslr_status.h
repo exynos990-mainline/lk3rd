2302b2e0d344049acc14dd60215e98fd180f18cd
Umer Uddin <umer.uddin@mentallysanemainliners.org>
2025-07-24 13:13:47 +0100

    lk3rd: Add configurable KASLR option
    So users can enable and disable it for debugging the kernel.
    
    Signed-off-by: Umer Uddin <umer.uddin@mentallysanemainliners.org>

---
+++ lib/lk3rd/include/lk3rd/kaslr_status.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2025 Umer Uddin <umer.uddin@mentallysanemainliners.org>
+ *
+ * Use of this source code is governed by a MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT
+ *
+ */
+#pragma once
+
+int lk3rd_switch_kaslr_status(bool enable);
+int lk3rd_get_kaslr_status(void);
--- lib/lk3rd/rules.mk
+++ lib/lk3rd/rules.mk
@@ -5,6 +5,7 @@ MODULE := $(LOCAL_DIR)
 MODULE_SRCS += \
 	$(LOCAL_DIR)/display.c \
 	$(LOCAL_DIR)/fastboot_menu.c \
+	$(LOCAL_DIR)/kaslr_status.c \
 	$(LOCAL_DIR)/keys.c \
 	$(LOCAL_DIR)/mainline_quirks.c \
 	$(LOCAL_DIR)/persistent_storage.c \
